f6d38683f7415117c0575078fa98b2da2ff8c05b
Agustín Salas <agustin.salas@sagalid.cl>
2020-06-10 11:35:54 -0400

    Valida IP por geo

--- validaIp.py
+++ validaIp.py
@@ -1,1 +1,11 @@
 #!/bin/python3
+import requests
+
+def main():
+	print("Validando si la IP pertenece a una botnet mediante INCIBE.")
+	data = {}
+	headers = {"X_INTECO_WS_Request_Source":"api"}
+	get = requests.get("https://antibotnet.osi.es/api/wscheckip/es", data=data, headers=headers)
+	print(get.json())
+if __name__ == "__main__":
+	main()
